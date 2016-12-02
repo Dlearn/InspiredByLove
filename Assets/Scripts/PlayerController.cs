792050a66c0f1d1621e97916684ef7e1cb1c878d
YIM CHIA HUI <E0003264@u.nus.edu>
2016-12-03 01:05:25 +0800

    addedMousePtr
    Object follows mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed;
 	// Use this for initialization
+	public float speed;
+	private float actualDistance;
+	public float distance = 1.0f;
+	public bool useInitialCameraDistance = false;
 	void Start () {
-		
+
+		if (useInitialCameraDistance) {
+			Vector3 toObjectVector = transform.position - Camera.main.transform.position;
+			Vector3 linearDistanceVector = Vector3.Project (toObjectVector, Camera.main.transform.forward);
+			actualDistance = linearDistanceVector.magnitude;
+		} else {
+			actualDistance = distance;
+		}
 	}
 	
 	// Update is called once per frame
@@ -18,5 +28,10 @@ public class PlayerController : MonoBehaviour {
         GetComponent<Rigidbody2D>().velocity = movement*speed;
 
         GetComponent<Rigidbody2D>().position = new Vector2(GetComponent<Rigidbody2D>().position.x, GetComponent<Rigidbody2D>().position.y);
+
+
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = actualDistance;
+		transform.position = Camera.main.ScreenToWorldPoint (mousePos);
     }
 }
